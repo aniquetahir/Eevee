dd75ae46cf483bfdd64d7e74a5e7b23a353f3901
Anique <aniquetahir@gmail.com>
2019-05-02 12:00:00 -0700

    Google Auth

--- authentication/SignInMenu.js
+++ authentication/SignInMenu.js
@@ -138,7 +138,7 @@ class SignInMenu extends Component{
                 });
                 let location = await this.getLocation();
                 let response = await this.callAPI('https://teamup-cc-546.appspot.com/login',
-                    {name: results.user.name , email:results.user.email, location:location},
+                    {name: result.user.name , email:result.user.email, location:location},
                     'POST');
                 await AsyncStorage.setItem('user_details', JSON.stringify(result.user));
                 const {name, email, photoUrl} = result.user;
